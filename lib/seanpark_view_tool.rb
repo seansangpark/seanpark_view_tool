a402425be6ff333f695644f99f737a93516efbae
Sean Park <seansangpark@gmail.com>
2022-02-26 14:23:31 -0800

    Dev tool initial commit

---
+++ lib/seanpark_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require_relative "seanpark_view_tool/version"
+require_relative "seanpark_view_tool/renderer"
+
+
+module SeanparkViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
